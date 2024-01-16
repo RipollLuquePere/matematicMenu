24d2a46638384dfad5904d426805651e7d5ab055
Pere Ripoll <pereriluq@gmail.com>
2024-01-16 09:14:00 +0100

    commit inicial

---
+++ menuMatematic_ArturShakirov_PereRipoll/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace menuMatematic_ArturShakirov_PereRipoll
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
