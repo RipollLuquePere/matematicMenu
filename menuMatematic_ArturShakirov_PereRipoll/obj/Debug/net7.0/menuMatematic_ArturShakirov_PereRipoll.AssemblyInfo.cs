8a7f40741675abe23ffde51e4c308f189454d0ef
Artur <a2308760@institutmontilivi.cat>
2024-01-18 09:40:25 +0100

    Commit backup develop

--- menuMatematic_ArturShakirov_PereRipoll/obj/Debug/net7.0/menuMatematic_ArturShakirov_PereRipoll.AssemblyInfo.cs
+++ menuMatematic_ArturShakirov_PereRipoll/obj/Debug/net7.0/menuMatematic_ArturShakirov_PereRipoll.AssemblyInfo.cs
@@ -14,7 +14,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("menuMatematic_ArturShakirov_PereRipoll")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+<<<<<<< HEAD
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4994110a803630b2138caf023ffe70186abf254c")]
+>>>>>>> main
 [assembly: System.Reflection.AssemblyProductAttribute("menuMatematic_ArturShakirov_PereRipoll")]
 [assembly: System.Reflection.AssemblyTitleAttribute("menuMatematic_ArturShakirov_PereRipoll")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
